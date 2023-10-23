fe3f7255a301a33cbc9e9a18012608cffc69ab17
Srijit <srijitmishra@protonmail.com>
2023-10-23 11:25:33 +0530

    Tailwind practise

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "./Button";
+
+function App() {
+  const handleClick = () => {
+    console.log("Click!!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          secondary
+          outline
+          rounded
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
