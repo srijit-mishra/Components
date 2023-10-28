710917d0087293b3c8e00d41b1741db6f210cb85
Srijit <srijitmishra@protonmail.com>
2023-10-28 09:45:45 +0530

    Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,32 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => (
+    <th className="p-3" key={column.label}>
+      {column.label}
+    </th>
+  ));
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => (
+      <td className="p-3" key={column.label}>
+        {column.render(rowData)}
+      </td>
+    ));
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,55 @@
-import Table from '../components/Table';
+import Table from "../components/Table";
 
 function TablePage() {
+  const data = [
+    {
+      name: "Orange",
+      color: "bg-orange-500",
+      score: 5,
+    },
+    {
+      name: "Apple",
+      color: "bg-red-500",
+      score: 3,
+    },
+    {
+      name: "Banana",
+      color: "bg-yellow-500",
+      score: 4,
+    },
+    {
+      name: "Grape",
+      color: "bg-purple-500",
+      score: 2,
+    },
+    {
+      name: "Watermelon",
+      color: "bg-green-500",
+      score: 1,
+    },
+  ];
+
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sort: (a, b) => a.score - b.score,
+    },
+  ];
+
+  const keyFn = (fruit) => fruit.name;
+
   return (
     <div>
-      <Table />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
